126c79981fa021f277cdede5b2971c4fc8065b2b
balu-pinisety <manikantapandu225@gmail.com>
2021-07-04 13:43:38 +0530

    Added: Methods of push and display of elements in OperationStack.java

--- StackDS/com/Stacks/OperationStacks.java
+++ StackDS/com/Stacks/OperationStacks.java
@@ -2,8 +2,37 @@ package com.Stacks;
 
 /**
  * @author BALU
- *
+ * Ability to create a Stack of Elements using Linked List
+ * Ability to perform Stack Operations on the list
  */
-public class OperationStacks {
-
+public class OperationStacks<T extends Comparable <T>> {
+	
+	NodeStacks<T> head;
+	
+	public OperationStacks() {
+		head = null;
+	}
+	
+	/**
+	 * Pushing a new element to it
+	 * @param args
+	 */
+	public void push(T data) {
+		NodeStacks<T> newNode = new NodeStacks<T>(data);
+		newNode.next=head;
+		head = newNode;
+	}
+	
+	/**
+	 * Displaying the elements in the Stack
+	 */
+	public void display() {
+		NodeStacks<T> temp = head;
+		System.out.print("Stack Sequence is :\n  ");
+		while(temp!=null) {
+			System.out.print(temp.data+"\n  ");
+			temp=temp.next;
+		}
+	}
+	
 }
